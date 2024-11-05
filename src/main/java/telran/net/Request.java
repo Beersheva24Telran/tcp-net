7c9f59d5fda4ff664f6f1ac45a1ca39f98cff49d
Yuragranovsky <yuragranovsky@gmail.com>
2024-11-05 17:54:07 +0200

    tcp-net tool initial code

---
+++ src/main/java/telran/net/Request.java
@@ -0,0 +1,13 @@
+package telran.net;
+
+import org.json.*;
+import static telran.net.TcpConfigurationProperties.*;
+public record Request(String requestType, String requestData) {
+    @Override
+    public String toString(){
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put(REQUEST_TYPE_FIELD, requestType);
+        jsonObj.put(REQUEST_DATA_FIELD, requestData);
+        return jsonObj.toString();
+    }
+}
